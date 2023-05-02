75d49794a71b2b10256c6aa5e21f2d11e9566597
AbdulMoeezImran <moeezimran6@gmail.com>
2023-05-02 06:20:58 +0500

    Updating Code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { searchRobots } from './reducers';
 import 'tachyons';
 import App from './containers/App';
+
+const store = createStore(searchRobots)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App/>
+    </Provider>
   </React.StrictMode>
 );
 
