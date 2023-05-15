e9c261291d59b875e99c83f76fc3132d07eada35
unknown <moeezimran6@gmail.com>
2023-05-15 16:00:26 -0700

    Switching Project to Redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from "redux-logger";
+import { searchRobots, requestRobots } from './reducers';
+import thunkMiddleware from 'redux-thunk';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { searchRobots } from './reducers';
-import 'tachyons';
 import App from './containers/App';
+import './index.css';
+import 'tachyons';
 
-const store = createStore(searchRobots)
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
